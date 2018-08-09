b39fb73a00580d14955f0c15343cb5a8454c1446
fbeaudin <francois@beaudin.co>
2018-08-09 16:55:41 -0400

    Changements
    Smooth Scrolling, Media queries, Enrichissement du texte.

--- scriptBeaudin.js
+++ scriptBeaudin.js
@@ -11,6 +11,7 @@ $(function(){
         $('#entreprise').hide();
     });
 
+//Icones dans à propos
     $('.hideshow').hide();
 
 
@@ -49,6 +50,36 @@ $(function(){
 
     $("#modalBtn").click(function(){
         $("#soumission").modal({backdrop: static});
+    });
+
+//Smooth Scrolling test.
+// Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 0});
+
+// Add smooth scrolling on all links inside the navbar
+    $("#myNavbar a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+
+        } // End if
+
     });
 
 
